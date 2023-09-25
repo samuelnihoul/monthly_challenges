ea0d89a18193f48e285ad9594194818411396506
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-25 17:43:45 +0200

    25

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
+monthly_challenges={
+    "january":"Eat no meat for the entire month!",
+    "february":"Walk for at least 20 minutes every day!",
+        "march":"Learn Django for at least 20 minutes every day!",
+    "april":"Eat no meat for the entire month!",
+    "may":"Walk for at least 20 minutes every day!",
+        "june":"Learn Django for at least 20 minutes every day!",
+    "july":"Eat no meat for the entire month!",
+    "august":"Walk for at least 20 minutes every day!",
+        "september":"Learn Django for at least 20 minutes every day!",
+    "october":"Eat no meat for the entire month!",
+    "november":"Walk for at least 20 minutes every day!",
+        "december":"Learn Django for at least 20 minutes every day!"
+}
+
 def monthly_challeng_by_number(request,month):
-    return HttpResponse(month)
+    return HttpResponse(monthly_challenges[month-1])
 
 def monthly_challenge(request,month):
-    challenge_text=None
-    if month=='january':
-        challenge_text="Eat no meat for the entire month!"
-    elif month=='february':
-        challenge_text='Walk for at least 20 minutes every day!'
-    elif month=='march':
-        challenge_text='Learn Django for at least 20 minutes every day!'
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text=monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound('This month is not found')
