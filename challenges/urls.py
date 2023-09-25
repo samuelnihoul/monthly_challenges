8e665f866e4642d0768d75f96ec4d86aebf744ea
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-25 17:04:13 +0200

    v22

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("january", views.index)
+    path("january", views.index),
+    path("february",views.february),
+    path("march",views.march)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse('It Works')
+def monthly_challenge(request,month):
+    challenge_text=None
+    if month=='january':
+        challenge_text="Eat no meat for the entire month!"
+    elif month=='february':
+        challenge_text='Walk for at least 20 minutes every day!'
+    elif month=='march':
+        challenge_text='Learn Django for at least 20 minutes every day!'
+    return HttpResponse()
